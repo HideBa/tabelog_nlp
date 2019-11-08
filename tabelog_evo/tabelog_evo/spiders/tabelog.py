78e28e8cb2ad0f3140bf9e66b39ae15eb4555623
Yutooo <yuuto3039@gmail.com>
2019-11-08 22:47:02 +0900

    ある程度かけた、動作確認まだ

--- tabelog_evo/tabelog_evo/spiders/tabelog.py
+++ tabelog_evo/tabelog_evo/spiders/tabelog.py
@@ -10,7 +10,7 @@ class TabelogSpider(CrawlSpider):
     食べログスクレイピングスパイダー
     東京の寿司屋さんの口コミををスクレイピングする
     """
-    self.store_id_num = 1
+    store_id = 0
     name = 'tabelog'
     allowed_domains = ['tabelog.com']
     start_urls = ['https://tabelog.com/tokyo/rstLst/sushi/?Srt=D&SrtT=rt&sort_mode=1']
@@ -27,6 +27,7 @@ class TabelogSpider(CrawlSpider):
             item = TabelogEvoItem()
             href = store["href"]
             item['link'] = href
+            self.store_id += 1
             request = scrapy.Request(
                 href,
                 callback=self.parse_detail
@@ -37,7 +38,7 @@ class TabelogSpider(CrawlSpider):
     def parse_detail(self, response):
         """
         店の詳細ページのパーシング
-        口コミページに飛ぶ
+        口コミページに移行
         """
         if response.status_code != requests.codes.ok:
             print(f'error:not found{ response }')
@@ -48,7 +49,7 @@ class TabelogSpider(CrawlSpider):
         soup = BeautifulSoup(response.body, 'html.parser')
         store_name_tag = soup.find('h2', class_='display-name')
         store_name = store_name_tag.span.string
-        print('{}→店名：{}'.format(self.store_id_num, store_name.strip()), end='')
+        print('{}→店名：{}'.format(self.store_id, store_name.strip()), end='')
         item['store_name'] = store_name.strip()
 
         # お寿司屋以外を除外
@@ -59,7 +60,138 @@ class TabelogSpider(CrawlSpider):
 
         if store_head_list[0].text not in {'寿司'}:
             print('お寿司屋さんではないので処理対象外')
-            self.store_id_num -= 1
+            self.store_id -= 1
             return
+        
+        # 評価点数取得
+        rating_score_tag = soup.find('b', class_='c-rating__val')
+        rating_score = rating_score_tag.span.string
+        print('  評価点数：{}点'.format(rating_score), end='')
+        item['store_score'] = rating_score
+
+        # 評価点数が存在しない店舗は除外
+        if rating_score == '-':
+            print('  評価がないため処理対象外')
+            self.store_id -= 1
+            return
+        
+        landd_tag = soup.find('div', class_='rstinfo-table__budget')
+        
+        lunch = landd_tag.find('em', class_='gly-b-lunch')
+        dinner = landd_tag.find('em', class_='gly-b-dinner')
+        try:
+            item['lunch_price'] = lunch.string
+        except:
+            item['lunch_price'] = ''
+        try:
+            item['dinner_price'] = dinner.string
+        except:
+            item['dinner_price'] = ''
+        
+        #print('　昼：{} 夜：{}'.format(self.lunch_price, self.dinner_price), end='')
+
+        # レビュー一覧URL取得
+        #<a class="mainnavi" href="https://tabelog.com/tokyo/A1304/A130401/13143442/dtlrvwlst/"><span>口コミ</span><span class="rstdtl-navi__total-count"><em>60</em></span></a>
+        review_tag_id = soup.find('li', id="rdnavi-review")
+        review_tag = review_tag_id.a.get('href')
+
+        # レビュー件数取得
+        print('  レビュー件数：{}'.format(review_tag_id.find('span', class_='rstdtl-navi__total-count').em.string), end='')
+        item['review_cnt'] = review_tag_id.find('span', class_='rstdtl-navi__total-count').em.string
+        
+        request = scrapy.Request(
+                review_tag,
+                callback=self.parse_review
+                )
+        request.meta['item'] = item
+        yield request
+
+
+    def parse_review(self, response):
+        """
+        口コミ一覧ページのパーシング
+        口コミ詳細ページに移行
+        """
+        #r = requests.get(response)
+        if response.status_code != requests.codes.ok:
+            print(f'error:not found{ response }')
+            return
+        item = response.meta['item']
+
+        soup = BeautifulSoup(response.body, 'html.parser')
+
+        review_url_list = soup.find_all('div', class_='rvw-item') # 口コミ詳細ページURL一覧
+        
+        if len(review_url_list) == 0:
+            return
+
+        for url in review_url_list:
+            review_detail_url = 'https://tabelog.com' + url.get('data-detail-url')
+            #print('\t口コミURL：', review_detail_url)
+
+            # 口コミのテキストを取得
+            self.get_review_text(review_detail_url)
+            self.i += 1
+        
+
+    def get_review_text(self, response):
+        """
+        口コミ詳細ページのパーシング
+        次の口コミへ
+        """
+        if response.status_code != requests.codes.ok:
+            print(f'error:not found{ response }')
+            return
+        item = response.meta['item']
+        
+        # 評価の内訳取得
+        # 料理味、サービス、雰囲気、CP、酒ドリンク
+        points = soup.find('ul', class_='rvw-item__ratings-dtlscore')
+        self.points = []
+        for li in points.find_all('li'):
+            self.points.append(li.strong.text)
+        if len(self.points) < 5:
+            self.points = ['-', '-', '-', '-', '-']
+        self.cuisine = self.points[0]
+        self.service = self.points[1]
+        self.atmos = self.points[2]
+        self.cp = self.points[3]
+        self.drink = self.points[4]
+        #print('\n料理: {} サービス: {} 雰囲気: {} CP: {} 酒: {}'.format(self.cuisine, self.service, self.atmos, self.cp, self.drink), end='')
+
+        score_tag = soup.find('p', class_='rvw-item__single-ratings-total')
+        score = score_tag.b.string
+        #print(' 口コミ評価点数：{}点'.format(score))
+        self.score = score
+
+        # 評価点数が存在しない店舗は除外
+        # if score == '-':
+        #     print('  評価がないため処理対象外')
+        #     self.store_id_num -= 1
+        #     return
+        
+        print("{}個めの口コミ取得完了".format(self.i))
+        # Review取得
+        review = soup.find_all('div', class_='rvw-item__rvw-comment')#reviewが含まれているタグの中身をすべて取得
+        if len(review) == 0:
+            review = ''
+        else:
+            review = review[0].p.text.strip() # strip()は改行コードを除外する関数
+            # reviewの1回目訪問のみ取得、複数取得するためにはfor i in range(len(review))でいけるはず...
+
+        #print('\t\t口コミテキスト：', review)
+        #self.review = review
+        if self.lunch:
+            self.lunch_review = review
+            self.dinner_review = ''
+        
+        if self.dinner:
+            self.lunch_review = ''
+            self.dinner_review = review
+
+
+
+
             
+
 
