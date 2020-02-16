f1596b045a41a62999281e129158d7d5f9f4f4a1
HideBa <baba.papa1120.ba@gmail.com>
2020-02-16 22:33:18 +0900

    add score history model

---
+++ sushi_proj/sushi_app/models/growth_rate_model.py
@@ -0,0 +1,19 @@
+# from django.db import models
+# from .store_model import Store
+# from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+# class GrowthRate(models.Model):
+#     class Meta:
+#         verbose_name = 'growth_rate'
+#         verbose_name = 'growth_rate'
+#     id = models.CharField('review_dinnerID', max_length=10, primary_key=True)
+#     growthRate
+#     store = models.ForeignKey(
+#         Store,
+#         verbose_name='store',
+#         null=True,
+#         on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.content[:30]
